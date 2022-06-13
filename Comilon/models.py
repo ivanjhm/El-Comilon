c4605624000402908c523cb29bcdb95de42bf7ed
Maria Acosta <mar.carvallo@duocuc.cl>
2022-06-13 11:36:15 -0400

    subir

--- Comilon/models.py
+++ Comilon/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Resgistro(models.Model):
+    rut = models.CharField(max_length=10,primary_key=True, verbose_name='Rut')
+    nombres = models.CharField(max_length=50, verbose_name='Nombre')
+    apellidos = models.CharField(max_length=50, verbose_name='Apellido')
+    correo = models.CharField(max_length=100, verbose_name='correo electrónico')
+    clave = models.CharField(max_length=16, verbose_name='Clave')
+    
+    def __str__(self):
+        return self.nombre
