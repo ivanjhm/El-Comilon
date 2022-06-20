e7d20a641aea9594d94a7a7af6b2cd27dccb7256
ivanjhm <101402648+ivanjhm@users.noreply.github.com>
2022-06-19 22:00:38 -0400

    eliminado

--- Comilon/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Rol, Usuario, Cliente, Produto
-
-# Register your models here.
-admin.site.register(Rol)
-admin.site.register(Usuario)
-admin.site.register(Cliente)
-admin.site.register(Produto)
